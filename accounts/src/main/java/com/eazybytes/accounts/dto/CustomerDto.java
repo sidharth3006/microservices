24814f06e100001cb9cd3a41a1b6832a1ec7675d
Sidharth <sidharth2020sharma@gmail.com>
2026-04-07 23:11:40 +0530

    Create DTO's

---
+++ accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -0,0 +1,17 @@
+package com.eazybytes.accounts.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+public class CustomerDto {
+    private String name;
+
+    private String email;
+
+    private String mobileNumber;
+
+}
