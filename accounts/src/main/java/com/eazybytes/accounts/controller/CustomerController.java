a6af30e06aed7030ccfeceefa75287e20fc7846f
Sidharth <sidharth2020sharma@gmail.com>
2026-05-07 10:01:10 +0530

    Setup Gatewy and routing

--- accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/CustomerController.java
@@ -12,14 +12,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(
         name = "CRUD REST APIs for Customer in EazyBank",
@@ -31,6 +30,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     private final ICustomerService iCustomerService;
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 
     public CustomerController(ICustomerService iCustomerService){
         this.iCustomerService = iCustomerService;
@@ -55,10 +56,11 @@ public class CustomerController {
     }
     )
     @GetMapping("/fetchCustomerDetails")
-    public ResponseEntity<CustomerDetailsDto> fetchAccountDetails(@RequestParam
+    public ResponseEntity<CustomerDetailsDto> fetchAccountDetails(@RequestHeader("eazybank-correlation-id") String correlationId, @RequestParam
                                                            @Pattern(regexp="(^$|[0-9]{10})",message = "mobile number must be 10 digits")
                                                            String mobileNumber){
-        CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber);
+        logger.debug("eazyBank-correlation-id found: {}", correlationId);
+        CustomerDetailsDto customerDetailsDto = iCustomerService.fetchCustomerDetails(mobileNumber,correlationId);
         return ResponseEntity.status(HttpStatus.OK).body(customerDetailsDto);
     }
 }
--- cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -15,6 +15,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
@@ -38,6 +40,8 @@ import org.springframework.web.bind.annotation.*;
 public class CardsController {
 
     private ICardsService iCardsService;
+
+    private static final Logger logger = LoggerFactory.getLogger(CardsController.class);
 
     public CardsController(ICardsService iCardsService) {
         this.iCardsService = iCardsService;
@@ -98,9 +102,10 @@ public class CardsController {
             )
     })
     @GetMapping("/fetch")
-    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestHeader("eazybank-correlation-id") String correlationId,@RequestParam
                                                      @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                      String mobileNumber) {
+        logger.debug("eazyBank-correlation-id found:{}",correlationId);
         CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
